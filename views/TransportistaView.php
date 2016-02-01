f6a17f7661a4caba0dfda4d1c0305fe86beb9fa8
Maria Calderon <mfce16@gmail.com>
2016-01-31 22:08:01 -0500

    Mafer-View/TransportistaView.php

---
+++ views/TransportistaView.php
@@ -0,0 +1,122 @@
+<?php
+session_start();
+?>
+
+<!doctype html>
+<html lang="es">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width; initial-scale=1.0"> 
+<title>Sistema SGL</title>
+    <!-- Estilos CSS vinculados -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    
+</head>
+
+<body>
+  <div class="container">
+	<div class="page-header">
+       <h1>Transportista</h1>
+    </div>
+   <form class="form-horizontal">
+ 
+    <div class="form-group">
+        <label class="control-label col-xs-3" >Ruc:</label>
+        <div class="col-xs-2">
+            <input type="tel" class="form-control" placeholder="Ruc">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-3">Nombre:</label>
+        <div class="col-xs-9">
+            <input type="text" class="form-control" placeholder="Nombre">
+        </div>
+    </div>
+    
+	<div class="form-group">
+        <label class="control-label col-xs-3" >Dirección:</label>
+        <div class="col-xs-9">
+            <input type="text" class="form-control" placeholder="Direccion">
+        </div>
+    </div>
+    
+    
+	<div class="form-group">
+        <label class="control-label col-xs-3" >Telefono 1:</label>
+        <div class="col-xs-2">
+            <input type="tel" class="form-control" placeholder="Telefono 1">
+        </div>
+		<label class="control-label col-xs-2" >Telefono 2:</label>
+		<div class="col-xs-2">
+            <input type="tel" class="form-control" placeholder="Telefono 2">
+        </div>
+    </div>
+	
+	
+	<div class="form-group">
+        <label class="control-label col-xs-3" >Celular 1:</label>
+        <div class="col-xs-2">
+            <input type="tel" class="form-control" placeholder="Celular 1">
+        </div>
+		<label class="control-label col-xs-2" >Celular 2:</label>
+        <div class="col-xs-2">
+            <input type="tel" class="form-control" placeholder="Celular 2">
+        </div>
+    </div>
+	
+	<div class="form-group">
+        <label class="control-label col-xs-3" >Contacto 1:</label>
+        <div class="col-xs-3">
+            <input type="tel" class="form-control" placeholder="Contacto 1">
+        </div>
+		<label class="control-label col-xs-2" >Contacto 2:</label>
+        <div class="col-xs-3">
+            <input type="tel" class="form-control" placeholder="Contacto 2">
+        </div>
+    </div>
+	
+	
+    <div class="form-group">
+        <label class="control-label col-xs-3" >Tipo:</label>
+        <div class="col-xs-2 select">
+           <input type="text" class="form-control" list="tipo" placeholder="Tipo" >
+		   <datalist id="tipo">
+			<option value="Juridico" label="Juridico">
+			<option value="Natural" label="Natural">
+		   </datalist>
+			<!--<input type="text" class="form-control" placeholder="Estado">-->
+        </div>
+    </div>		
+	
+	<div class="form-group">
+        <label class="control-label col-xs-3" >Contrato:</label>
+        <div class="col-xs-2 select">
+           <input type="text" class="form-control" list="tipo" placeholder="Contrato" >
+		   <datalist id="Contrato">
+			<option value="Fijo" label="Fijo">
+			<option value="Temporal" label="Temporal">
+		   </datalist>
+			<!--<input type="text" class="form-control" placeholder="Estado">-->
+        </div>
+    </div>		
+    
+    <br>
+    <div class="form-group">
+        <div class="col-xs-offset-3 col-xs-9">
+            <input type="submit" class="btn btn-primary" value="Ingresar">
+            <input type="submit" class="btn btn-primary" value="Modificar">
+			<input type="submit" class="btn btn-primary" value="Eliminar">
+			<input type="reset" class="btn btn-primary" value="Limpiar">
+			
+			
+        </div>
+    </div>
+  </form>
+</div>
+
+<!-- Js vinculados -->
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="../js/responsive.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
