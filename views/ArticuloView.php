f4e3054b3f44174abd387536ba2c737c5857ad5a
PaolaGomez <paojgome@espol.edu.ec>
2016-01-31 20:03:55 -0500

    PaolaGomez

---
+++ views/ArticuloView.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="es">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width; initial-scale=1.0"> 
+<title>Sistema SGL</title>
+    <!-- Estilos CSS vinculados -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+   
+</head>
+
+<body>
+  <div class="container">
+	<div class="page-header">
+       <h1>Articulo</h1>
+    </div>
+   <form class="form-horizontal">
+ 
+    <div class="form-group">
+        <label class="control-label col-xs-3">Codigo:</label>
+        <div class="col-xs-2">
+            <input type="text" class="form-control" placeholder="Codigo">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-3">Nombre:</label>
+        <div class="col-xs-9">
+            <input type="text" class="form-control" placeholder="Nombre">
+        </div>
+    </div>
+
+<div class="form-group">
+        <label class="control-label col-xs-3" >Descripcion:</label>
+        <div class="col-xs-2 select">
+           <textarea name="message" cols="40" rows="6" required></textarea>
+        </div>
+    </div>
+
+    <div class="form-group">
+        <label class="control-label col-xs-3" >Unidad de medida:</label>
+        <div class="col-xs-2">
+            <input type="tel" class="form-control" placeholder="Unidad">
+        </div>
+    </div>
+
+    
+    <div class="form-group">
+        <label class="control-label col-xs-3" >Estado:</label>
+       <div class="col-xs-9">
+            <select id=Estado>
+		<option value="">ACTIVO</option>
+  		<option value="">INACTIVO</option>
+
+  	    </select>
+        </div>
+
+        
+    </div>		
+    
+	
+
+    <br>
+    <div class="form-group">
+        <div class="col-xs-offset-3 col-xs-9">
+            <input type="submit" class="btn btn-primary" value="Ingresar">
+            <input type="submit" class="btn btn-primary" value="Modificar">
+			<input type="submit" class="btn btn-primary" value="Eliminar">
+			<input type="reset" class="btn btn-primary" value="Limpiar">
+			
+        </div>
+    </div>
+
+	
+
+  </form>
+</div>
+
+<!-- Js vinculados -->
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="../js/responsive.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
