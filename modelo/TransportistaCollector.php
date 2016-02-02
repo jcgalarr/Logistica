4491c71063b97f3c3b0c829d7848c964cd30f0a8
PaolaGomez <paojgome@espol.edu.ec>
2016-02-01 20:43:21 -0500

    paolagomez

--- modelo/TransportistaCollector.php
+++ modelo/TransportistaCollector.php
@@ -8,42 +8,46 @@ class TransportistaCollector extends conexion
   
   function showTransportista() {
     $rows = self::$db->getRows("SELECT * FROM transportista ");        
-    $arrayAreaTrabajo= array();        
+    $arrayTransportista= array();        
     foreach ($rows as $c){
-      $aux = new Transportista($c{'ruc'},$c{'nombre'});
+      $aux = new Transportista($c{'ruc'},$c{'nombre'},$c{'direccion'},$c{'telefono1'},$c{'telefono2'},$c{'numcelular1'},$c{'numcelular2'});
+
       array_push($arrayTransportista, $aux);
     }
     return $arrayTransportista;        
   }
  
-  function showTransportista($id) {
-    $row = self::$db->getRow("SELECT * FROM transportista where ruc = $id");   
+  function showTransportistaId($id) {
+    //echo $id;
+    $row = self::$db->getRow("SELECT * FROM transportista where ruc = '$id'");   
     //print_r ($row);     
-    $Transportista = new Transportista($row{'ruc'},$row{'nombre'});
+    $Transportista = new Transportista($row{'ruc'},$row{'nombre'},$row{'direccion'},$row{'telefono1'},$row{'telefono2'},$row{'numcelular1'},$row{'numcelular2'});
     return $Transportista;        
   }
 
-  function updateTransportista($id, $nombre,$direccion) {
-    $update = self::$db->getRow("Update transportista set nombre='$nombre',direccion='$direccion' where ruc=$id");             
+
+ 
+
+
+  function updateTransportista($ruc,$nombre,$direccion,$telefono1,$telefono2,$numcelular1,$numcelular2) {
+	
+	//echo $numcelular2;
+    $update = self::$db->getRow("Update transportista set nombre='$nombre',direccion='$direccion',telefono1='$telefono1',telefono2='$telefono2',numcelular1='$numcelular1',numcelular2='$numcelular2' where ruc='$ruc'");             
  return 1;  
   }
 
   function deleteTransportista($id) {
-   // $delete = self::$db->deleteRow("Delete from area_trabajo where id_area_trabajo=$id");
- $delete = self::$db->getRow("Delete from transportista where ruc=$id");   
+
+ $delete = self::$db->getRow("Delete from transportista where ruc= '$id'");   
    return 1;          
   }
 
-  function insertTransportista($ruc,$nombre,$direccion,$telefono1,$telefono2,$numcelular1,$numcelular2,$tipo,$contrato) {
-    $new_row = self::$db->getRow("Insert into transportista (ruc,nombre,direccion,telefono1,telefono2,numcelular1,numcelular2,tipo,contrato) values ('$ruc','$nombre','$direccion','$telefono1','$telefono2','$numcelular1','$numcelular2','$tipo','$contrato'),");
+  function insertTransportista($ruc,$nombre,$direccion,$telefono1,$telefono2,$numcelular1,$numcelular2) {
+    $new_row = self::$db->getRow("Insert into transportista (ruc,nombre,direccion,telefono1,telefono2,numcelular1,numcelular2) values ('$ruc','$nombre','$direccion','$telefono1','$telefono2','$numcelular1','$numcelular2')");
  return 1;               
   }
 
-  //function selectMax() {
-  //  $row = self::$db->getRow("SELECT MAX(ruc) from transportista");  
-  //  $max = $row{'max'};
-//return $max;
-  //}
+
 
 }
 ?>
