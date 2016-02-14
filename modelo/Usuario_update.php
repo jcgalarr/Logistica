f4fbf78a23c2f3eaae51b4b0e508195b96273690
Maria Calderon <mfce16@gmail.com>
2016-02-14 18:06:47 -0500

    Mafer-modelo/Usuario_update.php
    Actualizacion

---
+++ modelo/Usuario_update.php
@@ -0,0 +1,27 @@
+<?php
+ session_start();
+ include_once('UsuarioCollector.php');
+ 
+ $codigo = $_POST['txtcodigo'];
+ $nombre = $_POST['txtnombre'];
+ $apellido = $_POST['txtapellido'];
+ $cargo = $_POST['combocargo'];
+ $departamento = $_POST['combodepartamento'];
+ $bodega = $_POST['combobodega'];
+ $cedula = $_POST['txtcedula'];
+ $login = $_POST['txtlogin'];
+ $contrasena = $_POST['txtcontrasena'];
+ $estado = $_POST['comboestado'];
+ $tipousu = $_POST['combotipo'];
+
+ $UsuarioCollectorObj = new UsuarioCollector();
+ $ObjUsuario = $UsuarioCollectorObj->validaCedula($cedula);
+ 
+ if ( $ObjUsuario->getcedula() != '') {
+	echo"<script>alert('Cédula ya existe!');window.location.href=\"../views/UsuaView.php\"</script>";		
+	}
+	else {
+		$UsuarioCollectorObj->insertChofer($codigo,$nombre,$apellido,(int)$cargo,(int)$departamento,(int)$bodega,$cedula,$login,$contrasena,$estado,$tipousu);
+		echo"<script>alert('Registro Grabado con éxito');window.location.href=\"../views/UsuaView.php\"</script>";
+	}
+?>
