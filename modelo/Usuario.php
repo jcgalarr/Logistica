35554e714ffd9e50834d039e7352dac905da7af3
Maria Calderon <mfce16@gmail.com>
2016-02-14 17:58:57 -0500

    Mafer-modelo/Usuario.php
    Actualizacion

--- modelo/Usuario.php
+++ modelo/Usuario.php
@@ -2,30 +2,34 @@
 
 class Usuario
 {
-    private $codigo;
+  
+  private $codigo;
 	private $nombre;
 	private $apellido;
-	private $permisos;
-    private $privilegios;
 	private $cargo;
 	private $departamento;
 	private $bodega;
 	private $cedula;
-	private $fechacrea;
-	private $fechamodi;
+	private $login;
+	private $contrasena;
+  private $estado;
+  private $tipousu;
    
-    function __construct($codigo,$nombre,$apellido,$permisos,$privilegios,$cargo,$departamento,$bodega,$cedula,$fechacrea,$fechamodi) {
-       $this->codigo = $codigo;
+    function __construct($codigo,$nombre,$apellido,$cargo,$departamento,$bodega
+      ,$cedula,$login,$contrasena,$estado,$tipousu) {
+      
+     $this->codigo = $codigo;
 	   $this->nombre = $nombre;
 	   $this->apellido = $apellido;
-	   $this->permisos = $permisos;
-       $this->privilegios = $privilegios;
 	   $this->cargo = $cargo;
 	   $this->departamento = $departamento;
 	   $this->bodega = $bodega;
 	   $this->cedula = $cedula;
-	   $this->fechacrea = $fechacrea;
-	   $this->fechamodi = $fechamodi;
+	   $this->login = $login;
+	   $this->contrasena = $contrasena;
+     $this->estado = $estado;
+     $this->tipousu = $tipousu;
+
      }
 
      function setcodigo($codigo){
@@ -36,7 +40,7 @@ class Usuario
        return $this->codigo;
      } 
 	 
-	 function setnombre($nombre){
+	   function setnombre($nombre){
        $this->nombre = $nombre;
      }
  
@@ -44,7 +48,7 @@ class Usuario
        return $this->nombre;
      } 
 	 
-	 function setapellido($apellido){
+	   function setapellido($apellido){
        $this->apellido = $apellido;
      }
  
@@ -52,31 +56,15 @@ class Usuario
        return $this->apellido;
      } 
 	 
-	 function setpermisos($permisos){
-       $this->permisos = $permisos;
-     }
- 
-     function getpermisos(){
-       return $this->permisos;
-     } 
-
-     function setprivilegios($privilegios){
-       $this->privilegios = $privilegios;
-     } 
-
-     function getprivilegios(){
-       return $this->privilegios;
-     } 
-	 
-	 function setcargo($cargo){
+	   function setcargo($cargo){
        $this->cargo = $cargo;
      }
  
      function getcargo(){
        return $this->cargo;
      } 
-	 
-	 function setdepartamento($departamento){
+
+    function setdepartamento($departamento){
        $this->departamento = $departamento;
      }
  
@@ -84,38 +72,55 @@ class Usuario
        return $this->departamento;
      }
 
-	 function setbodega($bodega){
+     function setbodega($bodega){
        $this->bodega = $bodega;
-     }
- 
+     } 
+
      function getbodega(){
        return $this->bodega;
-     }
+     } 
 	 
-	 function setcedula($cedula){
+	  function setcedula($cedula){
        $this->cedula = $cedula;
      }
  
      function getcedula(){
        return $this->cedula;
-     }
+     } 
 	 
-	 function setfechacrea($fechacrea){
-       $this->fechacrea = $fechacrea;
+  	 function setlogin($login){
+       $this->login = $login;
      }
  
-     function getfechacrea(){
-       return $this->fechacrea;
+     function getlogin(){
+       return $this->login;
      }
-	 
-	 function setfechamodi($fechamodi){
-       $this->fechamodi = $fechamodi;
+	  
+	  function setcontrasena($contrasena){
+       $this->contrasena = $contrasena;
      }
  
-     function getfechamodi(){
-       return $this->fechamodi;
+     function getcontrasena(){
+       return $this->contrasena;
+     }
+	 
+	   function setestado($estado){
+       $this->estado = $estado;
+     }
+ 
+     function getestado(){
+       return $this->estado;
+     }
+	 
+	   function settipousu($tipousu){
+       $this->tipousu = $tipousu;
+     }
+ 
+     function gettipousu(){
+       return $this->tipousu;
      }
 
 }
 
 ?> 
+
