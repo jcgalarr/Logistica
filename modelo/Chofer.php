aa7ef60666e16507dc532907a1d2c05a66a62da6
Maria Calderon <mfce16@gmail.com>
2016-02-14 17:41:19 -0500

    Mafer-modelo/Chofer.php
    Actualizacion

--- modelo/Chofer.php
+++ modelo/Chofer.php
@@ -1,87 +1,90 @@
 <?php
 
-class chofer
+class Chofer
 {
     private $codigo;
     private $nombre;
     private $apellido;
     private $licencia;
     private $categoria_lic;
-    private $numceular1;
+    private $numcelular1;
     private $recordpolicial;
     private $transportista;
   
-    function __construct($codigo, $nombre,$apellido,$licencia,$categoria_lic,$numceular1,$recordpolicial,$transportista) {
+    function __construct($codigo,$nombre,$apellido,$licencia,$categoria_lic,$numcelular1,$recordpolicial,$transportista) {
        $this->codigo = $codigo;
        $this->nombre = $nombre;
        $this->apellido = $apellido;
        $this->licencia = $licencia;
        $this->categoria_lic = $categoria_lic;
-       $this->numceular1 = $numceular1;
+       $this->numcelular1 = $numcelular1;
        $this->recordpolicial = $recordpolicial;
        $this->transportista = $transportista;
      }
 
-   
-     function setcodigo($codigo){
+    function setcodigo($codigo){
        $this->codigo = $codigo;
-     }
+    }
  
-     function getcodigo(){
+    function getcodigo(){
        return $this->codigo;
-     } 
+    } 
 
-     function setnombre($nombre){
+    function setnombre($nombre){
        $this->nombre = $nombre;
-     } 
+    } 
 
-     function getnombre(){
+    function getnombre(){
        return $this->nombre;
-     } 
+    } 
 
-
- function setapellido($apellido){
+    function setapellido($apellido){
        $this->apellido = $apellido;
-     } 
+    } 
 
-     function getapellido(){
+    function getapellido(){
        return $this->apellido;
-     } 
- function setlicencia($licencia){
+    } 
+
+    function setlicencia($licencia){
        $this->licencia = $licencia;
-     } 
+    } 
 
-     function getlicencia(){
+    function getlicencia(){
        return $this->licencia;
-     }   
- function setcategoria_lic($categoria_lic){
+    }  
+
+    function setcategoria_lic($categoria_lic){
        $this->categoria_lic = $categoria_lic;
-     } 
+    } 
 
-     function getcategoria_lic(){
+    function getcategoria_lic(){
        return $this->categoria_lic;
-     } 
- function setnumceular1($numceular1){
-       $this->numceular1 = $numceular1;
-     } 
+    }
 
-     function getnumceular1(){
-       return $this->numceular1;
-     } 
- function setrecordpolicial($recordpolicial){
+    function setnumcelular1($numcelular1){
+       $this->numcelular1 = $numcelular1;
+    } 
+
+    function getnumcelular1(){
+       return $this->numcelular1;
+    }
+
+    function setrecordpolicial($recordpolicial){
        $this->recordpolicial = $recordpolicial;
-     } 
+    } 
 
-     function getrecordpolicial(){
+    function getrecordpolicial(){
        return $this->recordpolicial;
-     } 
- function settransportista($transportista){
-       $this->transportista = $transportista;
-     } 
+    } 
 
-     function gettransportista(){
+    function settransportista($transportista){
+       $this->transportista = $transportista;
+    } 
+
+    function gettransportista(){
        return $this->transportista;
-     } 
+    } 
 }
 
 ?> 
