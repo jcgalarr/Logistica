21f3654a10d44c717bfa7084ba0d544275fd6e3f
Antonio Ayala <afayala@espol.edu.ec>
2016-02-15 01:59:42 -0500

    Antonio Ayala

--- views/ColaReposicionView.php
+++ views/ColaReposicionView.php
@@ -1,3 +1,10 @@
+<?php
+ session_start();
+ include_once('../modelo/Almacena.php');
+ include_once('../modelo/AlmacenaCollector.php');
+ $AlmacenaCollectorObj = new AlmacenaCollector();
+?>
+
 <!doctype html>
 <html lang="es">
 <head>
@@ -11,43 +18,113 @@
 
 <body>
   <div class="container">
+  <div class="col-xs-offset-10 col-xs-9">
+    <br>
+    <input  type="button" value="Menú" OnClick="window.location='../views/ViewMenu.php'" class="btn btn-primary" class="">
+  </div>
   <div class="page-header">
-       <h1>Cola Reposicion-2/tcl</h1>
+       <h1>Almacenamiento</h1>
     </div>
 	<div class="page-header">
        <h1>Almacenar Pallet</h1>
     </div>
-   <form class="form-horizontal">
+   <form id="frm_almacen" name="frm_almacen" action ="../modelo/Almacena_insert.php" method ="post" class="form-horizontal">
  
     <div class="form-group">
         <label class="control-label col-xs-3">Pallet:</label>
         <div class="col-xs-2">
-            <input type="text" class="form-control" placeholder="Pallet">
+            <input onkeypress="return ValidNumber(event);" MaxLength="3" Onblur="validapallet();" type="tel" id="txtpallet" name="txtpallet" class="form-control" placeholder="Pallet" value = "101" disabled >
         </div>
     </div>
     <div class="form-group">
         <label class="control-label col-xs-3">No.:</label>
         <div class="col-xs-2">
-            <input type="text" class="form-control" placeholder="No.">
+            <input onkeypress="return ValidNumber(event);" MaxLength="6" type="tel" id="txtnumero" name="txtnumero" class="form-control" placeholder="No.">
         </div>
     </div>
-	<div class="page-header">
+    <div class="form-group">
+        <label class="control-label col-xs-3">Producto:</label>
+        <div class="col-xs-2">
+            <input onkeypress="return ValidNumber(event);" MaxLength="9" type="tel" id="txtproducto" name="txtproducto" class="form-control" placeholder="No.">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-3">Cantidad:</label>
+        <div class="col-xs-2">
+            <input onkeypress="return ValidNumber(event);" MaxLength="6" type="tel" id="txtcantidad" name="txtcantidad" class="form-control" placeholder="No.">
+        </div>
+    </div>
+    <div class="page-header">
        <h1>Destino-Dejar en</h1>
     </div>
     <div class="form-group">
-        <label class="control-label col-xs-3" >No.:</label>
+        <label class="control-label col-xs-3" >Ubicacion:</label>
         <div class="col-xs-2">
-            <input type="tel" class="form-control" placeholder="No.">
+            <input onkeypress="return ValidNumber(event);" MaxLength="9" type="tel" id="txtubicacion" name="txtubicacion" class="form-control" placeholder="No.">
         </div>
     </div>
 
-  
+    <br>
+    <div class="form-group">
+        <div class="col-xs-offset-3 col-xs-9">
+            <input type="button" OnClick="insertar()" class="btn btn-primary" value="Almacenar">
+            <input type="reset" class="btn btn-primary" value="Limpiar">
+	</div>
+    </div>
   </form>
 </div>
+
+<script > 
+
+function validapallet()
+    {   var num = document.getElementById("txtpallet").value;
+              
+            if (num.length<3)
+            {   alert('Pallet debe ser de 3 digitos');
+                document.getElementById("txtpallet").focus();
+		return false;
+            }
+            
+        
+    }
+
+function insertar()
+    {
+        if (document.getElementById("txtpallet").value=="") {
+            alert('Campo Pallet es Obligatorio');
+	    document.getElementById("txtpallet").focus();
+            return false;         
+        }
+	if (document.getElementById("txtnumero").value=="") {
+            alert('Campo Numero es Obligatorio');
+	    document.getElementById("txtnumero").focus();
+            return false;         
+        }
+	if (document.getElementById("txtubicacion").value=="") {
+            alert('Campo Ubicacion es Obligatorio');
+	    document.getElementById("txtubicacion").focus();
+            return false;         
+        }
+	if (document.getElementById("txtproducto").value=="") {
+            alert('Campo Producto es Obligatorio');
+	    document.getElementById("txtproducto").focus();
+            return false;         
+        }
+	if (document.getElementById("txtcantidad").value=="") {
+            alert('Campo Cantidad es Obligatorio');
+	    document.getElementById("txtcantidad").focus();
+            return false;         
+        }  
+	alert('Grabado exitosamente!');
+        document.frm_almacen.submit();
+    }
+</script>
 
 <!-- Js vinculados -->
     <script src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script src="../js/responsive.js"></script>
     <script src="../js/bootstrap.min.js"></script>
+    <script language ="javascript" type = "text/javascript" src= "../js/funcionesgenerales.js">
+</script>
 </body>
 </html>
--- views/ControlEntradasView.php
+++ views/ControlEntradasView.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+include_once ('conexion.php');
+include_once("../modelo/BodegaCollector.php");
+include_once('../modelo/Incorpora.php');
+include_once('../modelo/IncorporaCollector.php');
+$BodegaCollectorObj = new BodegaCollector();
+$IncorporaCollectorObj = new IncorporaCollector();
+?>
 <!doctype html>
 <html lang="es">
 <head>
@@ -11,43 +20,91 @@
 
 <body>
   <div class="container">
+  <div class="col-xs-offset-10 col-xs-9">
+    <br>
+    <input  type="button" value="Menú" OnClick="window.location='../views/ViewMenu.php'" class="btn btn-primary" class="">
+  </div>
   <div class="page-header">
-       <h1>Control de entradas</h1>
+       <h1>Incorporacion de Solicitud de Despacho</h1>
     </div>
 	
-   <form class="form-horizontal">
- 
+    <form id="frm_incorpora" name="frm_incorpora" action ="../modelo/Incorpora_insert.php" method ="post" class="form-horizontal">
     <div class="form-group">
-        <label class="control-label col-xs-2">PTA:</label>
+        <label class="control-label col-xs-2">Bodega:</label>
         <div class="col-xs-2">
-            <input type="text" class="form-control" placeholder="PTA">
-        </div>
-		 <label class="control-label col-xs-2">Pal:</label>
-        <div class="col-xs-2">
-            <input type="text" class="form-control" placeholder="Pal">
+            <select type="text"  id="combobodega" name="combobodega" >
+		<?php  
+
+foreach ($BodegaCollectorObj->showBodega() as $c){
+?>
+		<option value="<?php echo $c->getdescripcion() ?>"><?php echo $c->getdescripcion() ?></option>
+<?php  
+}
+?>
+  	    </select>
         </div>
     </div>
    
 	
     <div class="form-group">
-        <label class="control-label col-xs-2" >Estadistico</label>
+        <label class="control-label col-xs-2" >Caddie</label>
         <div class="col-xs-2">
-            <input type="text" class="form-control" placeholder="Estadistico">
+            <input type="text" id="txtcaddie" name="txtcaddie" MaxLength="6" onkeypress="return ValidNumber(event);" class="form-control" placeholder="Caddie">
+        </div>
+    </div>
+
+  <div class="form-group">
+        <label class="control-label col-xs-2" >Producto</label>
+        <div class="col-xs-2">
+            <input type="text" id="txtproducto" name="txtproducto" MaxLength="9" onkeypress="return ValidNumber(event);" class="form-control" placeholder="Producto">
         </div>
     </div>
 
   <div class="form-group">
         <label class="control-label col-xs-2" >Cantidad</label>
         <div class="col-xs-2">
-            <input type="text" class="form-control" placeholder="Cantidad">
+            <input type="text" class="form-control" placeholder="Cantidad" onkeypress="return ValidNumber(event);" id="txtcantidad" name="txtcantidad">
         </div>
+    </div>
+
+   <br>
+    <div class="form-group">
+        <div class="col-xs-offset-3 col-xs-9">
+            <input type="button" OnClick="insertar()" class="btn btn-primary" value="Grabar">
+            <input type="reset" class="btn btn-primary" value="Limpiar">
+	</div>
     </div>
   </form>
 </div>
 
+
+<script > 
+
+function insertar()
+    {
+        if (document.getElementById("txtcaddie").value=="") {
+            alert('Campo Caddie es Obligatorio');
+	    document.getElementById("txtcaddie").focus();
+            return false;         
+        }
+	if (document.getElementById("txtproducto").value=="") {
+            alert('Campo Producto es Obligatorio');
+	    document.getElementById("txtproducto").focus();
+            return false;         
+        }
+	if (document.getElementById("txtcantidad").value=="") {
+            alert('Campo Cantidad es Obligatorio');
+	    document.getElementById("txtcantidad").focus();
+            return false;         
+        }
+	alert('Grabado exitosamente!');
+        document.frm_incorpora.submit();
+    }
+</script>
 <!-- Js vinculados -->
     <script src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script src="../js/responsive.js"></script>
     <script src="../js/bootstrap.min.js"></script>
+    <script language ="javascript" type = "text/javascript" src= "../js/funcionesgenerales.js"></script>
 </body>
 </html>
